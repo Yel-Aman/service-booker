79d6d4ed654d7eac6a6a5ad1c02a56818182de21
Yel-Aman <73169471+Yel-Aman@users.noreply.github.com>
2026-07-31 10:39:48 +0500

    fixed translation Service bug

--- services/tests.py
+++ services/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeTranslationTests(TestCase):
+    def test_home_how_it_works_text_is_translated_to_kazakh(self):
+        self.client.post(
+            reverse('set_language'),
+            {'language': 'kk', 'next': reverse('home')},
+        )
+
+        response = self.client.get(reverse('home'))
+
+        self.assertContains(response, 'Қызметті табыңыз')
+        self.assertContains(response, 'Санатты таңдап, қолайлы орынды табыңыз')
+
+    def test_home_how_it_works_text_is_translated_to_english(self):
+        self.client.post(
+            reverse('set_language'),
+            {'language': 'en', 'next': reverse('home')},
+        )
+
+        response = self.client.get(reverse('home'))
+
+        self.assertContains(response, 'Find a service')
+        self.assertContains(response, 'Choose a category and find the right place')
